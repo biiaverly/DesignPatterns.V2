e298e8b685023db14d8ce56ca597b5dbb2340136
Bia Verly <biaverly@bia.verly@oliveiratrust.com.br>
2023-07-18 10:33:15 -0300

    Decorator: Decorator

--- app/Services/Imposto/Imposto2Aliquotas.php
+++ app/Services/Imposto/Imposto2Aliquotas.php
@@ -5,20 +5,20 @@ namespace App\Services\Imposto;
 use App\Interfaces\ImpostoInterface;
 use App\Services\Orcamento\Orcamento;
 
-abstract class Imposto2Aliquotas implements ImpostoInterface
+abstract class Imposto2Aliquotas extends ImpostoInterface
 {
-    
+    /// Decorator
+    public function realizaCalculoEspecifico(Orcamento $orcamento): float
+    {
+        if ($this->deveAplicarTaxaMaxima($orcamento)) {
+            return $this->calculaTaxaMaxima($orcamento);
+        }
+
+        return $this->calculaTaxaMinima($orcamento);
+    }
+
     abstract protected function deveAplicarTaxaMaxima(Orcamento $orcamento): bool;
     abstract protected function calculaTaxaMaxima(Orcamento $orcamento): float;
     abstract protected function calculaTaxaMinima(Orcamento $orcamento): float;
     
-    public function calculaImposto(Orcamento $orcamento): float
-    {
-        if ($this->deveAplicarTaxaMaxima($orcamento))
-        {
-            return $this->calculaTaxaMaxima($orcamento);
-        }
-        return $this->calculaTaxaMinima($orcamento);
-
-    }
 }
