2d0964152e57c31a8e84a65d972343080533b579
Bia Verly <biaverly@bia.verly@oliveiratrust.com.br>
2023-08-01 11:02:57 -0300

    Facade: Facade

--- app/Services/Descontos/CalculadoraDesconto.php
+++ app/Services/Descontos/CalculadoraDesconto.php
@@ -7,16 +7,22 @@ namespace  App\Services\Descontos;
 use App\Services\Orcamento\Orcamento;
 
 
-class CalculadoraDesconto 
+class CalculadoraDesconto
 {
-    public function calculaDescontos(Orcamento $orcamento):float
+    public function calculaDescontos(Orcamento $orcamento): float
     {
         $cadeiaDeDescontos = new Desconto500itensmais(
             new Desconto500reaismais(
                 new SemDesconto()
-                ) 
-            );
-                
-        return $cadeiaDeDescontos->calculaDesconto($orcamento);
+            )
+        );
+
+        $descontoCalculado = $cadeiaDeDescontos->calculaDesconto($orcamento);
+
+        $logDesconto = new LogDesconto();
+        $logDesconto->informar($descontoCalculado);
+
+        return $descontoCalculado;
+
     }
 }
